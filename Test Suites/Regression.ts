16417ae884ce758b1453ca972f2e59df44775c38
Loan Tran <loan.tran@katalon.com>
2021-04-09 16:08:28 +0700

    update

--- Test Suites/Regression.ts
+++ Test Suites/Regression.ts
@@ -17,4 +17,64 @@
       <isRun>true</isRun>
       <testCaseId>Test Cases/Recording</testCaseId>
    </testCaseLink>
+   <testCaseLink>
+      <guid>7d62f343-c3b3-4945-b0a5-053c1c8ece75</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Create a new ticket by API</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>1b6d04f5-8f9d-415b-95ac-4bcdcdc3f793</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>a2a45360-7819-4d23-bebb-3b01e315548e</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>6504583c-06fc-47bf-be01-03d3c47fe94c</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>8d3de896-8ba4-4c1e-9812-c123d373c258</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>1c5bdbed-cc66-4fec-8db6-31bb9b695d3e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Create and update a ticket by both API and UI</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>64bd85d7-7795-4cb6-a05b-3bf26fb66127</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>a6dc30f6-4ea0-45b5-89de-e05adf9add85</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>6f0a04e2-9584-4019-aa84-5dc9f837e1d3</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>33b33e69-742d-471f-9454-ccdf4dfdb5d9</variableId>
+      </variableLink>
+   </testCaseLink>
 </TestSuiteEntity>
